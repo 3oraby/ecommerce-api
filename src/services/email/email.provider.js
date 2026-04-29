8ca47d4c6e390d3ae7f5901d98fbe0f262c4c490
3oraby <mohamedahmed21102003@gmail.com>
2026-04-29 05:10:43 +0300

    convert smtp to api key

--- src/services/email/email.provider.js
+++ src/services/email/email.provider.js
@@ -1,22 +1,16 @@
 const nodemailer = require("nodemailer");
 const pug = require("pug");
 const path = require("path");
+const ApiError = require("../../utils/apiError");
+const HttpStatus = require("../../enums/httpStatus.enum");
+const SibApiV3Sdk = require("sib-api-v3-sdk");
 
-const createTransporter = () => {
-  if (process.env.NODE_ENV === "production") {
-    return nodemailer.createTransport({
-      host: process.env.GMAIL_HOST,
-      port: process.env.GMAIL_PORT,
-      auth: {
-        user: process.env.GMAIL_USERNAME,
-        pass: process.env.GMAIL_PASSWORD,
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-    });
-  }
+const renderTemplate = (templateName, data) => {
+  const templatePath = path.join(__dirname, "templates", `${templateName}.pug`);
+  return pug.renderFile(templatePath, data);
+};
 
+const createDevTransporter = () => {
   return nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
@@ -30,27 +24,43 @@ const createTransporter = () => {
   });
 };
 
-const transporter = createTransporter();
+const brevoClient = SibApiV3Sdk.ApiClient.instance;
+brevoClient.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
 
-const renderTemplate = (templateName, data) => {
-  const templatePath = path.join(__dirname, "templates", `${templateName}.pug`);
-  return pug.renderFile(templatePath, data);
-};
+const brevoEmailApi = new SibApiV3Sdk.TransactionalEmailsApi();
 
 exports.sendEmail = async ({ to, subject, templateName, data }) => {
   try {
     const html = renderTemplate(templateName, data);
 
-    const mailOptions = {
-      from: process.env.EMAIL_FROM || "noreply@ecommerce-api.com",
-      to,
-      subject,
-      html,
-    };
+    if (process.env.NODE_ENV !== "production") {
+      const transporter = createDevTransporter();
 
-    return await transporter.sendMail(mailOptions);
+      return await transporter.sendMail({
+        from: process.env.EMAIL_FROM || "dev@ecommerce.com",
+        to,
+        subject,
+        html,
+      });
+    }
+
+    await brevoEmailApi.sendTransacEmail({
+      sender: {
+        email: process.env.EMAIL_FROM,
+        name: "Ecommerce App",
+      },
+      to: [{ email: to }],
+      subject,
+      htmlContent: html,
+    });
+
+    return true;
   } catch (error) {
-    console.error(`Failed to send email to ${to}:`, error);
-    throw new Error("Email provider encountered an error");
+    console.error("EMAIL ERROR:", error.response?.body || error.message);
+
+    throw new ApiError(
+      "Email provider encountered an error",
+      HttpStatus.InternalServerError,
+    );
   }
 };
