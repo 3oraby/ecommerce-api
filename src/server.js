88b830b458e07ebc709ee9af35574aa76d878d6a
3oraby <mohamedahmed21102003@gmail.com>
2026-08-07 17:35:33 +0300

    add logger

--- src/server.js
+++ src/server.js
@@ -17,37 +17,33 @@ connectRedis();
 
 sequelize
   .authenticate()
-  .then(() => console.log("DB connection successful"))
-  .catch((err) => console.error("Unable to connect to DB:", err));
+  .then(() => logger.info("DB connection successful"))
+  .catch((err) => logger.error("Unable to connect to DB:", err));
 
 queueService
   .connect()
   .then(() => {
-    console.log("RabbitMQ connection initialized.");
+    logger.info("RabbitMQ connection initialized.");
     return startNotificationConsumer();
   })
-  .catch((err) => console.error("Failed to initialize RabbitMQ:", err));
+  .catch((err) => logger.error("Failed to initialize RabbitMQ:", err));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   logger.info(`Server running on port ${port}`);
-
-  logger.warn("Redis reconnecting...");
-
-  logger.error("Database connection failed");
 });
 
 const gracefulShutdown = async () => {
-  console.log("Starting graceful shutdown...");
+  logger.info("Starting graceful shutdown...");
   try {
     await queueService.close();
     await sequelize.close();
     server.close(() => {
-      console.log("Server closed.");
+      logger.info("Server closed.");
       process.exit(0);
     });
   } catch (error) {
-    console.error("Error during graceful shutdown:", error);
+    logger.error("Error during graceful shutdown:", error);
     process.exit(1);
   }
 };
